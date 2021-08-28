21a2d9c15ac7c5165e17a469a8b7787aaf2bc160
geeta kumari <geetakumari3840@gmail.com>
2021-08-28 10:22:11 +0530

    cday2808

--- lib/test_page/fatch_data_internet.dart
+++ lib/test_page/fatch_data_internet.dart
@@ -10,7 +10,7 @@ Future<Album> fetchAlbum() async {
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
-    // then parse the JSON.
+    // then parse the JSON
     return Album.fromJson(jsonDecode(response.body));
   } else {
     // If the server did not return a 200 OK response,
